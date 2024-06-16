e9c473f3715674ed2214979cf2aeb105ed1ccfbb
Simisoluwa <38164663+Simisoluwa@users.noreply.github.com>
2024-06-16 17:56:42 +0100

    Update config.py

--- src/bot/config.py
+++ src/bot/config.py
@@ -1,9 +1,9 @@
 class Config(object):
     LOGGER = True
-    DATABASE_URI ='postgresql://postgres:24111999@localhost:5432/groupref'
+    DATABASE_URI ='postgresql+psycopg2://postgres:^r;/(5#+ki|F:8Rk@/telegram-referral-bot?host=/cloudsql/scorefam-bot:us-central1:telegram-referral-bot'
     LOG_CHANNEL = -1001410504549
     SUDO_USERS=[431108047,972650691,1773748036,1508254655,1776321910,1669215589,1853258941]
     BOT_TOKEN='1393190801:AAFSRCGOQAajiyY7SE5kxTDTcaPDecOQAjs'
     WORKERS = 8
-    SUPPORT_CHANNEL='NanoDogecoin'
+    SUPPORT_CHANNEL='scbotts'
     
